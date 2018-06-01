21f3b5207f8d4de0c38548bf607e8a17f43c1b7e
aljand <aljand321@gmail.com>
2018-06-01 17:01:14 -0400

    seminario

---
+++ app/src/main/java/com/example/alejandro/labseminario/ItemMenu/OnLoadCompleImg.java
@@ -0,0 +1,9 @@
+package com.example.alejandro.labseminario.ItemMenu;
+
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+public interface OnLoadCompleImg {
+
+    public void OnLoadCompleImgResult(ImageView img, int position, Bitmap imgsourceimg);
+}
